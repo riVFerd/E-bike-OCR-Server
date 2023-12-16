cd5aa2d528790a6ca6ff6df08e7ab1e9141e63b3
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-12-16 09:42:40 +0700

    Update improve text extraction accuracy

--- ocr_ktm.py
+++ ocr_ktm.py
@@ -7,21 +7,20 @@ from ktm_model import KTMModel
 
 
 def ocr_raw(image):
-    image = cv2.resize(image, (50 * 16, 500))
+    # image = cv2.resize(image, (50 * 16, 500))
 
-    img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     # img_gray = cv2.equalizeHist(img_gray)
     # img_gray = cv2.fastNlMeansDenoising(img_gray, None, 3, 7, 21)
-    cv2.fillPoly(img_gray, pts=[np.asarray([(540, 150), (540, 499), (798, 499), (798, 150)])], color=(255, 255, 255))
-    th, threshed = cv2.threshold(img_gray, 127, 255, cv2.THRESH_TRUNC)
-    result_raw = pytesseract.image_to_string(threshed, lang="ind")
+    # cv2.fillPoly(img_gray, pts=[np.asarray([(540, 150), (540, 499), (798, 499), (798, 150)])], color=(255, 255, 255))
+    # th, threshed = cv2.threshold(img_gray, 127, 255, cv2.THRESH_TRUNC)
+    result_raw = pytesseract.image_to_string(image, lang="ind")
 
     return result_raw
 
 
 def main(image):
-    result_raw = ocr_raw(image)
-
+    result_raw = ocr_raw(cv2.imread('image.jpg'))
     nim = ''
     nama = ''
     ttl = ''
@@ -30,28 +29,35 @@ def main(image):
 
     lines = result_raw.split('\n')
 
-    for i in lines:
-        print(i)
+    # for i in lines:
+    #     print(i)
 
+    # remove empty lines
+    lines = list(filter(lambda x: x != '', lines))
 
     # Find NIM and its index
     nim_index = next((i for i, line in enumerate(lines) if re.match(r'^\d+$', line)), None)
-
     if nim_index is not None:
         nim = lines[nim_index]
 
-        # Extract other information based on the index of NIM
-        if nim_index + 1 < len(lines):
-            nama = lines[nim_index + 1]
+    # Extract other information based on the index of NIM
+    if nim_index + 1 < len(lines):
+        nama = lines[nim_index + 1]
 
-        if nim_index + 2 < len(lines):
-            ttl = lines[nim_index + 2]
+    if nim_index + 2 < len(lines):
+        ttl = lines[nim_index + 2]
 
-        if nim_index + 3 < len(lines):
-            jurusan = lines[nim_index + 3]
+    if nim_index + 3 < len(lines):
+        jurusan = lines[nim_index + 3]
 
-        if nim_index + 4 < len(lines):
-            alamat = ' '.join(lines[nim_index + 4:])
+    if nim_index + 4 < len(lines):
+        alamat = ' '.join(lines[nim_index + 4:])
+
+    # print('nim: ' + nim)
+    # print('nama: ' + nama)
+    # print('ttl: ' + ttl)
+    # print('jurusan: ' + jurusan)
+    # print('alamat: ' + alamat)
 
     return KTMModel(nim, nama, ttl, jurusan, alamat)
 
